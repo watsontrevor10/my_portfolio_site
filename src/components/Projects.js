4dc9ea800f59169712f565bf983ed17ac38b5202
Trevor Watson <watsontrevor10@gmail.com>
2020-02-26 16:45:27 -0700

    added YouTube tour embedded video, and simplified descriptions of Seekr.

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -30,14 +30,13 @@ const Projects = () => {
           <Card.Content>
             <Card.Header>
               DevPoint Seekr
-          </Card.Header>
+            </Card.Header>
             <Card.Meta>
               A jobs search tracking application
-          </Card.Meta>
+            </Card.Meta>
           </Card.Content>
           <Card.Content>
             <Image src={dpl_seekr} size='big' centered />
-            <br />
             <Card.Description>
               My team of 7 DevPoint Labs students built this to help job seekers keep track of the
               job search process without using a complex spreadsheet or sticky notes.
@@ -60,13 +59,12 @@ const Projects = () => {
             >
               <Seekr />
             </Modal>
-            <Label as='a' color='teal' image>
+            <Label as='a' color='teal' image attached='top right'>
               <a href='https://github.com/watsontrevor10/dpl-seekr' target='_blank' rel='noopener noreferrer'>
                 <Icon link name='github square' />
                 GitHub
               </a>
             </Label>
-
           </Card.Content>
         </Card>
 
@@ -75,28 +73,29 @@ const Projects = () => {
           <Card.Content>
             <Card.Header>
               Star Wars Fan Site
-          </Card.Header>
+            </Card.Header>
             <Card.Meta>
               Simple app using SWAPI.co API
-          </Card.Meta>
+            </Card.Meta>
           </Card.Content>
           <Card.Content>
             <Image src={star_wars} size='medium' />
             <Card.Description>
               DevPoint Labs project, built in the space of an afternoon.
-              The main goal was to be able to pull info via external API and have a simple UI to view data, while exploring a new CSS library.
-          </Card.Description>
+              The main goal was to be able to pull info via external API and have a simple UI to view data, 
+              while exploring a new CSS library.
+            </Card.Description>
           </Card.Content>
           <Card.Content>
             <Card.Meta>
               React.js | REST API | Grommet UI
-          </Card.Meta>
+            </Card.Meta>
             <br />
-            <Label as='a' color='teal' image>
+            <Label as='a' color='teal' image attached='top right'>
               <a href='https://github.com/watsontrevor10/star_wars_fan_site' target='_blank' rel='noopener noreferrer'>
                 <Icon link name='github square' />
                 GitHub
-          </a>
+              </a>
             </Label>
           </Card.Content>
         </Card>
--- src/components/Seekr.js
+++ src/components/Seekr.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Container, Embed, Divider, Header, List, Segment } from 'semantic-ui-react'
+import dplseekr from './assets/dpl-seeker.png'
 
 
 const Seekr = () => {
@@ -10,24 +11,27 @@ const Seekr = () => {
         as='h2'
         textAlign='center'
       >
-        Seekr by DevPoint Labs
+        DevPoint Seekr
       </Header>
-      <Embed>Placeholder for video</Embed>
+      <Embed
+        id='Dps7caZ1CaQ'
+        placeholder={dplseekr}
+        source='youtube'
+      />
       <Divider />
-      <Container>
+      {/* <Container> */}
         <p>
           Seekr was built by myself and 6 colleagues as part of the DevPoint Labs coding curriculum.
           As a team, we had about a month to complete the assignment, which was to build a job search tracking application.
-          This tool helps keep track of jobs that you are applying for as part of the job search in a Kanban board fashion.
+          This tool helps keep track of jobs that you are applying for as part of a job search in a Kanban board.
         </p>
         <p>
-          Inside of each job that you set up you have the ability to track info about the job, notes, tasks, contacts, and interviews.
+          For each job that you are applying for you have the ability to track info about the job, notes, tasks, contacts, and interviews.
         </p>
         <p>
-          There is also a home page with graphs and modules that you show you number of jobs applied for, stage of your applications,
-          and upcoming tasks and interviews
+          There is also a home page with graphs and modules that give a good overview of where you are at in the process and what you need to do next.
         </p>
-        <p>
+        {/* <p>
           While we worked together as a team or in pairs on most things, some of the modules I contributed to most were:
         </p>
         <List>
@@ -59,7 +63,7 @@ const Seekr = () => {
             </List.Content>
           </List.Item>
         </List>
-      </Container>
+      </Container> */}
     </Segment>
   )
 }
