639838cd7a7a6915b90e6b774c7e7ba0593676db
Trevor Watson <watsontrevor10@gmail.com>
2020-02-26 17:33:58 -0700

    made DomoPreso a modal popup from Domo card. Added experience component to take experience out of Home

--- src/components/DomoPreso.js
+++ src/components/DomoPreso.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Header, Embed, } from 'semantic-ui-react'
+import { Header, Container, } from 'semantic-ui-react'
 
 
 const DomoPreso = () => {
@@ -12,19 +12,20 @@ const DomoPreso = () => {
           Using NUVI social media data and DOMO
         </Header.Subheader>
       </Header>
-      <Embed autoplay='true'>
+      <Container>
         <iframe
           title='Domo Demo'
           src="https://docs.google.com/presentation/d/e/2PACX-1vRYCaZMGsMmkZoBCeSXRXd2wrXkZPy92AJI7k2YKdvbDCiCB2Vj5DpRGJCzxfwudwBMeMYh3kNbyT8Z/embed?start=true&loop=true&delayms=3000"
           frameborder="0"
-          width="1058"
-          height="823"
-          allowfullscreen="true"
-          mozallowfullscreen="true"
-          webkitallowfullscreen="true"
+          position='absolute'
+          height='850'
+          width='100%'
+          allowfullscreen="false"
+          mozallowfullscreen="false"
+          webkitallowfullscreen="false"
         >
         </iframe>
-      </Embed>
+      </Container>
     </>
   )
 }
